5c49be40115bd582b44641857ea82dd5ee54b819
hardBoxzy <89336980+hardBoxzy@users.noreply.github.com>
2026-07-06 02:50:35 +0900

    課題提出  関数をMDNで調べて使ってみよう

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const monthIndex = date.getMonth() + 1;
+const day = date.getDate();
+
+
+
+console.log(year+"年"+monthIndex+"月"+day+"日");
