f8ef2a421d48f84874b91c56a63a74ed4b4eff57
hardBoxzy <89336980+hardBoxzy@users.noreply.github.com>
2026-07-06 22:59:10 +0900

    課題提出  DOM操作を使用して文字表示を変えよう

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  // 選択されたラジオボタンの値を取得する
+  const text = document.getElementById('text');
+
+  text.textContent = 'ボタンをクリックしました';
+});
